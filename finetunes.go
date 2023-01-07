35363c71a140a5e8c51ec9b7d119a68c7610dd80
Arve Knudsen <arve.knudsen@gmail.com>
2023-01-07 15:22:49 +0100

    Add Client.RetrieveFineTune
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- finetunes.go
+++ finetunes.go
@@ -6,16 +6,17 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"path"
 )
 
-// FineTuneRequest represents a request to create an OpenAPI fine-tune.
+// FineTuneRequest represents a request to create an OpenAI fine-tune.
 type FineTuneRequest struct {
 	TrainingFile string `json:"training_file"`
 	Model        string `json:"model,omitempty"`
 	Suffix       string `json:"suffix,omitempty"`
 }
 
-// FineTuneResponse represents a response to a request for creating an OpenAPI fine-tune.
+// FineTuneResponse represents a response to a request for creating an OpenAI fine-tune.
 type FineTuneResponse struct {
 	ID     string          `json:"id"`
 	Model  string          `json:"model"`
@@ -30,6 +31,7 @@ type FineTuneEvent struct {
 	Message   string `json:"message"`
 }
 
+// CreateFineTune requests creating an OpenAI fine-tune.
 func (c *Client) CreateFineTune(ctx context.Context, request FineTuneRequest) (FineTuneResponse, error) {
 	b := bytes.NewBuffer(nil)
 	if err := json.NewEncoder(b).Encode(request); err != nil {
@@ -46,6 +48,36 @@ func (c *Client) CreateFineTune(ctx context.Context, request FineTuneRequest) (F
 	req.Header.Set("Content-Type", "application/json")
 
 	var resp FineTuneResponse
+	err = c.sendRequest(req, &resp)
+	return resp, err
+}
+
+// RetrieveFineTuneResponse represents a response to a request to retrieve a fine-tune from OpenAI.
+type RetrieveFineTuneResponse struct {
+	ID              string          `json:"id"`
+	Object          string          `json:"object"`
+	Model           string          `json:"model"`
+	CreatedAt       int64           `json:"created_at"`
+	Events          []FineTuneEvent `json:"events"`
+	FineTunedModel  string          `json:"fine_tuned_model"`
+	OrganizationID  string          `json:"organization_id"`
+	ResultFiles     []File          `json:"result_files"`
+	Status          string          `json:"status"`
+	ValidationFiles []File          `json:"validation_files"`
+	TrainingFiles   []File          `json:"training_files"`
+}
+
+// RetrieveFineTune requests the retrieval of an OpenAI fine-tune.
+func (c *Client) RetrieveFineTune(ctx context.Context, id string) (RetrieveFineTuneResponse, error) {
+	u := c.fullURL(path.Join("/fine-tunes", id))
+	req, err := http.NewRequest(http.MethodGet, u, nil)
+	if err != nil {
+		return RetrieveFineTuneResponse{}, fmt.Errorf("make %s request to %s: %w", http.MethodGet, u, err)
+	}
+
+	req = req.WithContext(ctx)
+
+	var resp RetrieveFineTuneResponse
 	err = c.sendRequest(req, &resp)
 	return resp, err
 }
